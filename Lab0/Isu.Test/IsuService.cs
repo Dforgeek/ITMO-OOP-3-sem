f9858489bc6abeee8ced38f01dd528a059741df6
Dforgeek <1812stepanovsky@gmail.com>
2022-09-16 18:25:27 +0300

    feat: add test 1

--- Lab0/Isu.Test/IsuService.cs
+++ Lab0/Isu.Test/IsuService.cs
@@ -1,3 +1,5 @@
+using Isu.Entities;
+using Isu.Models;
 using Xunit;
 
 namespace Isu.Test;
@@ -5,10 +7,22 @@ namespace Isu.Test;
 public class IsuService
 {
     [Fact]
-    public void AddStudentToGroup_StudentHasGroupAndGroupContainsStudent() { }
+    public void AddStudentToGroup_StudentHasGroupAndGroupContainsStudent()
+    {
+        var isu = new Services.IsuService();
+        var coolGroupName = new GroupName("M3203");
+        Group coolGroup = isu.AddGroup(coolGroupName);
+        var coolStudent = isu.AddStudent(coolGroup, "Michael Lopatin");
+
+        Assert.Contains(coolStudent, coolGroup.Students);
+    }
 
     [Fact]
-    public void ReachMaxStudentPerGroup_ThrowException() { }
+    public void ReachMaxStudentPerGroup_ThrowException()
+    {
+        var isu = new Services.IsuService();
+        isu.AddGroup()
+    }
 
     [Fact]
     public void CreateGroupWithInvalidName_ThrowException() { }
