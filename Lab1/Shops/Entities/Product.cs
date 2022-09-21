a42973b319f2b87a97747fad489c26a4f703c308
Dforgeek <1812stepanovsky@gmail.com>
2022-09-21 18:02:02 +0300

    feat: add entities, models

---
+++ Lab1/Shops/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using Shops.Exceptions;
+using Shops.Models;
+
+namespace Shops.Entities;
+
+public class Product
+{
+    public Product(string name, Price price, uint quantity = 1, Guid id = default)
+    {
+        Id = id == Guid.Empty ? Guid.NewGuid() : id;
+        if (string.IsNullOrEmpty(name))
+            throw new ShopException("Name consists of zero characters");
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public Guid Id { get; }
+    public string Name { get; }
+    public Price Price { get; private set; }
+    public uint Quantity { get; }
+
+    public void ChangePrice(Price newPrice)
+    {
+        Price = newPrice;
+    }
+}
---
+++ Lab1/Shops/Models/Price.cs
@@ -0,0 +1,15 @@
+﻿using Shops.Exceptions;
+
+namespace Shops.Models;
+
+public class Price
+{
+    public Price(decimal price)
+    {
+        if (price <= 0)
+            throw new ShopException($"Invalid price: {price}");
+        Value = price;
+    }
+
+    public decimal Value { get; }
+}
