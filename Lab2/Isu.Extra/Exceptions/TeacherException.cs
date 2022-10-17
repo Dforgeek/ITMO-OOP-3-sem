1151111f20c345f885bf792b7719d53b62e2a236
Dforgeek <1812stepanovsky@gmail.com>
2022-10-17 14:17:54 +0300

    feat: add few entities

---
+++ Lab2/Isu.Extra/Exceptions/TeacherException.cs
@@ -0,0 +1,14 @@
+﻿namespace Isu.Extra.Exceptions;
+
+public class TeacherException : Exception
+{
+    private TeacherException(string message)
+        : base(message)
+    {
+    }
+
+    public static TeacherException InvalidTeacherName(string message)
+    {
+        return new TeacherException(message);
+    }
+}
