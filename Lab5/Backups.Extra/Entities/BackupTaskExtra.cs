8cac1a40c28f606949ce30d3b62f3516d53a79fb
Dforgeek <1812stepanovsky@gmail.com>
2022-11-17 23:38:09 +0300

    feat: start memento

---
+++ Lab5/Backups.Extra/Entities/BackupTaskExtra.cs
@@ -0,0 +1,31 @@
+﻿using Backups.Entities;
+using Backups.Extra.Interfaces;
+using Backups.Interfaces;
+
+namespace Backups.Extra.Entities;
+
+public class BackupTaskExtra
+{
+    private readonly BackupTask _backupTask;
+    private IBackup _backup;
+
+    public BackupTaskExtra(IBackup backup, IRepository repository, IStorageAlgorithm storageAlgorithm, ILogger logger, IRestorePointControl restorePointControl, Guid id)
+    {
+        _backup = backup;
+        Logger = logger;
+        RestorePointControl = restorePointControl;
+        _backupTask = new BackupTask(_backup, repository, storageAlgorithm, id);
+    }
+
+    public Guid Id => _backupTask.Id;
+    public IRepository Repository => _backupTask.Repository;
+    public IStorageAlgorithm StorageAlgorithm => _backupTask.StorageAlgorithm;
+    public IRestorePointControl RestorePointControl { get; }
+    public string BackupTaskPath => _backupTask.BackupTaskPath;
+    public ILogger Logger { get; }
+
+    public BackupTaskExtraMemento GetMemento()
+    {
+        
+    }
+}
---
+++ Lab5/Backups.Extra/Entities/BackupTaskExtraMemento.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Common;
+using Backups.Entities;
+using Backups.Extra.Interfaces;
+using Backups.Interfaces;
+
+namespace Backups.Extra.Entities;
+
+public class BackupTaskExtraMemento
+{
+    private IBackup _backup;
+    private IRepository _repository;
+    private IStorageAlgorithm _storageAlgorithm;
+    private Guid _id;
+    private ILogger _logger;
+    private IRestorePointControl _restorePointControl;
+
+    public BackupTaskExtraMemento(IBackup backup, IRepository repository, IStorageAlgorithm storageAlgorithm, ILogger logger, IRestorePointControl restorePointControl, Guid id)
+    {
+        _backup = backup;
+        _id = id;
+        _storageAlgorithm = storageAlgorithm;
+        _repository = repository;
+        _restorePointControl = restorePointControl;
+        _logger = logger;
+    }
+
+    public BackupTaskExtra DownloadBackupTask()
+    {
+        return new BackupTaskExtra(_backup, _repository, _storageAlgorithm, _logger, _restorePointControl, _id);
+    }
+}
