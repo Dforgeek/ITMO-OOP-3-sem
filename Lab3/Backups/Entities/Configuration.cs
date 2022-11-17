7cdf8d24b7c9f5436ceadbd3e24438d8a6ed478b
Dforgeek <1812stepanovsky@gmail.com>
2022-11-17 23:55:05 +0300

    feat: add IConfiguration.cs

---
+++ Lab3/Backups/Entities/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using Backups.Interfaces;
+
+namespace Backups.Entities;
+
+public class Configuration
+{
+    public Configuration(IRepository repository, IStorageAlgorithm storageAlgorithm)
+    {
+        Repository = repository;
+        StorageAlgorithm = storageAlgorithm;
+    }
+
+    public IRepository Repository { get; }
+    public IStorageAlgorithm StorageAlgorithm { get; }
+}
---
+++ Lab3/Backups/Interfaces/IConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Backups.Interfaces;
+
+public interface IConfiguration
+{
+    public IRepository Repository { get; }
+    public IStorageAlgorithm StorageAlgorithm { get; }
+}
