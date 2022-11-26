cb61e6f165a903c97e4db1420c7b3428d222a101
Dforgeek <1812stepanovsky@gmail.com>
2022-11-26 13:09:39 +0300

    feat: fix BackupProxy

---
+++ Lab5/Backups.Extra/Entities/BackupProxyWithLogger.cs
@@ -0,0 +1,39 @@
+﻿using Backups.Entities;
+using Backups.Exceptions;
+using Backups.Extra.Interfaces;
+using Backups.Interfaces;
+
+namespace Backups.Extra.Entities;
+
+public class BackupProxyWithLogger : IBackup
+{
+    private IBackup _backup;
+
+    public BackupProxyWithLogger(IBackup backup, ILogger logger)
+    {
+        _backup = backup;
+        Logger = logger;
+    }
+
+    public ILogger Logger { get; }
+
+    public IReadOnlyCollection<RestorePoint> RestorePoints => _backup.RestorePoints;
+    public void AddRestorePoint(RestorePoint restorePoint)
+    {
+        _backup.AddRestorePoint(restorePoint);
+        Logger.Log($"Added RestorePoint: {restorePoint.Id}");
+    }
+
+    public RestorePoint GetRestorePoint(Guid id)
+    {
+        RestorePoint result = _backup.GetRestorePoint(id);
+        Logger.Log($"Found RestorePoint: {id}");
+        return result;
+    }
+
+    public void DeleteRestorePoint(Guid id)
+    {
+        _backup.DeleteRestorePoint(id);
+        Logger.Log($"Deleted RestorePoint {id}");
+    }
+}
